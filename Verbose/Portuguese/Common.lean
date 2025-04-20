19bdf0a5ab4c34b6ba013cf27a58f8cc92f19b27
lucasarruda12 <lucas123manoel4@gmail.com>
2025-04-20 15:42:10 -0300

    common

--- Verbose/Portuguese/Common.lean
+++ Verbose/Portuguese/Common.lean
@@ -2,33 +2,33 @@ import Verbose.Tactics.Common
 
 open Lean
 
-namespace Verbose.English
+namespace Verbose.Portuguese
 
 declare_syntax_cat appliedTo
-syntax "applied to " sepBy(term, " and ") : appliedTo
+syntax "aplicado em " sepBy(term, " e ") : appliedTo
 
 def appliedToTerm : TSyntax `appliedTo → Array Term
-| `(appliedTo| applied to $[$args]and*) => args
+| `(appliedTo| aplicado em $[$args]e*) => args
 | _ => default -- This will never happen as long as nobody extends appliedTo
 
 declare_syntax_cat usingStuff
-syntax " using " sepBy(term, " and ") : usingStuff
-syntax " using that " term : usingStuff
+syntax " usando " sepBy(term, " e ") : usingStuff
+syntax " usando que " term : usingStuff
 
 def usingStuffToTerm : TSyntax `usingStuff → Array Term
-| `(usingStuff| using $[$args]and*) => args
-| `(usingStuff| using that $x) => #[Unhygienic.run `(strongAssumption% $x)]
+| `(usingStuff| usando $[$args]e*) => args
+| `(usingStuff| usando que $x) => #[Unhygienic.run `(strongAssumption% $x)]
 | _ => default -- This will never happen as long as nobody extends appliedTo
 
 declare_syntax_cat maybeApplied
 syntax term (appliedTo)? (usingStuff)? : maybeApplied
 
 def maybeAppliedToTerm : TSyntax `maybeApplied → MetaM Term
-| `(maybeApplied| $e:term) => pure e
-| `(maybeApplied| $e:term $args:appliedTo) => `($e $(appliedToTerm args)*)
-| `(maybeApplied| $e:term $args:usingStuff) => `($e $(usingStuffToTerm args)*)
-| `(maybeApplied| $e:term $args:appliedTo $extras:usingStuff) =>
-  `($e $(appliedToTerm args)* $(usingStuffToTerm extras)*)
+| `(maybeApplied| $f:term) => pure f
+| `(maybeApplied| $f:term $args:appliedTo) => `($f $(appliedToTerm args)*)
+| `(maybeApplied| $f:term $args:usingStuff) => `($f $(usingStuffToTerm args)*)
+| `(maybeApplied| $f:term $args:appliedTo $extras:usingStuff) =>
+  `($f $(appliedToTerm args)* $(usingStuffToTerm extras)*)
 | _ => pure default -- This will never happen as long as nobody extends maybeApplied
 
 /-- Build a maybe applied syntax from a list of term.
@@ -37,52 +37,52 @@ and the second one is its main arguments. When there is a third element, it is
 to be the type of a prop argument. -/
 def listTermToMaybeApplied : List Term → MetaM (TSyntax `maybeApplied)
 | [x] => `(maybeApplied|$x:term)
-| [x, y] => `(maybeApplied|$x:term applied to $y)
-| [x, y, z] => `(maybeApplied|$x:term applied to $y using that $z)
-| x::y::l => `(maybeApplied|$x:term applied to $y:term using [$(.ofElems l.toArray),*])
+| [x, y] => `(maybeApplied|$x:term aplicado em $y)
+| [x, y, z] => `(maybeApplied|$x:term aplicado em $y usando que $z)
+| x::y::l => `(maybeApplied|$x:term aplicado em $y:term usando que [$(.ofElems l.toArray),*])
 | _ => pure ⟨Syntax.missing⟩ -- This should never happen
 
 declare_syntax_cat newStuff
 syntax (ppSpace colGt maybeTypedIdent)* : newStuff
-syntax maybeTypedIdent "such that" ppSpace colGt maybeTypedIdent : newStuff
-syntax maybeTypedIdent "such that" ppSpace colGt maybeTypedIdent " and "
+syntax maybeTypedIdent "tal que" ppSpace colGt maybeTypedIdent : newStuff
+syntax maybeTypedIdent "tal que" ppSpace colGt maybeTypedIdent " e "
        ppSpace colGt maybeTypedIdent : newStuff
 
 def newStuffToArray : TSyntax `newStuff → Array MaybeTypedIdent
 | `(newStuff| $news:maybeTypedIdent*) => Array.map toMaybeTypedIdent news
-| `(newStuff| $x:maybeTypedIdent such that $news:maybeTypedIdent) =>
+| `(newStuff| $x:maybeTypedIdent tal que $news:maybeTypedIdent) =>
     Array.map toMaybeTypedIdent #[x, news]
-| `(newStuff| $x:maybeTypedIdent such that $y:maybeTypedIdent and $z) =>
+| `(newStuff| $x:maybeTypedIdent tal que $y:maybeTypedIdent e $z) =>
     Array.map toMaybeTypedIdent #[x, y, z]
 | _ => #[]
 
 def listMaybeTypedIdentToNewStuffSuchThatEN : List MaybeTypedIdent → MetaM (TSyntax `newStuff)
 | [x] => do `(newStuff| $(← x.stx):maybeTypedIdent)
-| [x, y] => do `(newStuff| $(← x.stx):maybeTypedIdent such that $(← y.stx'))
-| [x, y, z] => do `(newStuff| $(← x.stx):maybeTypedIdent such that $(← y.stx) and $(← z.stx))
+| [x, y] => do `(newStuff| $(← x.stx):maybeTypedIdent tal que $(← y.stx'))
+| [x, y, z] => do `(newStuff| $(← x.stx):maybeTypedIdent tal que $(← y.stx) e $(← z.stx))
 | _ => pure default
 
 declare_syntax_cat newFacts
 syntax colGt namedType : newFacts
-syntax colGt namedType " and "  colGt namedType : newFacts
-syntax colGt namedType ", "  colGt namedType " and "  colGt namedType : newFacts
+syntax colGt namedType " e "  colGt namedType : newFacts
+syntax colGt namedType ", "  colGt namedType " e "  colGt namedType : newFacts
 
 def newFactsToArray : TSyntax `newFacts → Array NamedType
 | `(newFacts| $x:namedType) => #[toNamedType x]
-| `(newFacts| $x:namedType and $y:namedType) =>
+| `(newFacts| $x:namedType e $y:namedType) =>
     #[toNamedType x, toNamedType y]
-| `(newFacts| $x:namedType, $y:namedType and $z:namedType) =>
+| `(newFacts| $x:namedType, $y:namedType e $z:namedType) =>
     #[toNamedType x, toNamedType y, toNamedType z]
 | _ => #[]
 
 def newFactsToTypeTerm : TSyntax `newFacts → MetaM Term
 | `(newFacts| $x:namedType) => do
     namedTypeToTypeTerm x
-| `(newFacts| $x:namedType and $y) => do
+| `(newFacts| $x:namedType e $y) => do
     let xT ← namedTypeToTypeTerm x
     let yT ← namedTypeToTypeTerm y
     `($xT ∧ $yT)
-| `(newFacts| $x:namedType, $y:namedType and $z) => do
+| `(newFacts| $x:namedType, $y:namedType e $z) => do
     let xT ← namedTypeToTypeTerm x
     let yT ← namedTypeToTypeTerm y
     let zT ← namedTypeToTypeTerm z
@@ -92,21 +92,21 @@ def newFactsToTypeTerm : TSyntax `newFacts → MetaM Term
 open Tactic Lean.Elab.Tactic.RCases in
 def newFactsToRCasesPatt : TSyntax `newFacts → RCasesPatt
 | `(newFacts| $x:namedType) => namedTypeListToRCasesPatt [x]
-| `(newFacts| $x:namedType and $y:namedType) => namedTypeListToRCasesPatt [x, y]
-| `(newFacts|  $x:namedType, $y:namedType and $z:namedType) => namedTypeListToRCasesPatt [x, y, z]
+| `(newFacts| $x:namedType e $y:namedType) => namedTypeListToRCasesPatt [x, y]
+| `(newFacts|  $x:namedType, $y:namedType e $z:namedType) => namedTypeListToRCasesPatt [x, y, z]
 | _ => default
 
 declare_syntax_cat newObject
-syntax maybeTypedIdent "such that " maybeTypedIdent : newObject
-syntax maybeTypedIdent "such that " maybeTypedIdent colGt " and " maybeTypedIdent : newObject
+syntax maybeTypedIdent "tal que " maybeTypedIdent : newObject
+syntax maybeTypedIdent "tal que " maybeTypedIdent colGt " e " maybeTypedIdent : newObject
 
 def newObjectToTerm : TSyntax `newObject → MetaM Term
-| `(newObject| $x:maybeTypedIdent such that $new) => do
+| `(newObject| $x:maybeTypedIdent tal que $new) => do
     let x' ← maybeTypedIdentToExplicitBinder x
     -- TODO Better error handling
     let newT := (toMaybeTypedIdent new).2.get!
     `(∃ $(.mk x'), $newT)
-| `(newObject| $x:maybeTypedIdent such that $new₁ and $new₂) => do
+| `(newObject| $x:maybeTypedIdent tal que $new₁ e $new₂) => do
     let x' ← maybeTypedIdentToExplicitBinder x
     let new₁T := (toMaybeTypedIdent new₁).2.get!
     let new₂T := (toMaybeTypedIdent new₂).2.get!
@@ -116,64 +116,64 @@ def newObjectToTerm : TSyntax `newObject → MetaM Term
 -- TODO: create helper functions for the values below
 open Tactic Lean.Elab.Tactic.RCases in
 def newObjectToRCasesPatt : TSyntax `newObject → RCasesPatt
-| `(newObject| $x:maybeTypedIdent such that $new) => maybeTypedIdentListToRCasesPatt [x, new]
-| `(newObject| $x:maybeTypedIdent such that $new₁ and $new₂) => maybeTypedIdentListToRCasesPatt [x, new₁, new₂]
+| `(newObject| $x:maybeTypedIdent tal que $new) => maybeTypedIdentListToRCasesPatt [x, new]
+| `(newObject| $x:maybeTypedIdent tal que $new₁ e $new₂) => maybeTypedIdentListToRCasesPatt [x, new₁, new₂]
 | _ => default
 
 declare_syntax_cat facts
 syntax term : facts
-syntax term " and " term : facts
-syntax term ", " term " and " term : facts
-syntax term ", " term ", " term " and " term : facts
+syntax term " e " term : facts
+syntax term ", " term " e " term : facts
+syntax term ", " term ", " term " e " term : facts
 
 def factsToArray : TSyntax `facts → Array Term
 | `(facts| $x:term) => #[x]
-| `(facts| $x:term and $y:term) => #[x, y]
-| `(facts| $x:term, $y:term and $z:term) => #[x, y, z]
-| `(facts| $x:term, $y:term, $z:term and $w:term) => #[x, y, z, w]
+| `(facts| $x:term e $y:term) => #[x, y]
+| `(facts| $x:term, $y:term e $z:term) => #[x, y, z]
+| `(facts| $x:term, $y:term, $z:term e $w:term) => #[x, y, z, w]
 | _ => #[]
 
 def arrayToFacts : Array Term → CoreM (TSyntax `facts)
 | #[x] => `(facts| $x:term)
-| #[x, y] => `(facts| $x:term and $y:term)
-| #[x, y, z] => `(facts| $x:term, $y:term and $z:term)
-| #[x, y, z, w] => `(facts| $x:term, $y:term, $z:term and $w:term)
+| #[x, y] => `(facts| $x:term e $y:term)
+| #[x, y, z] => `(facts| $x:term, $y:term e $z:term)
+| #[x, y, z, w] => `(facts| $x:term, $y:term, $z:term e $w:term)
 | _ => default
 
-end Verbose.English
+end Verbose.Portuguese
 
 /-- Convert an expression to a `maybeApplied` syntax object, in `MetaM`. -/
-def Lean.Expr.toMaybeApplied (e : Expr) : MetaM (TSyntax `maybeApplied) := do
-  let fn := e.getAppFn
+def Lean.Expr.toMaybeApplied (f : Expr) : MetaM (TSyntax `maybeApplied) := do
+  let fn := f.getAppFn
   let fnS ← PrettyPrinter.delab fn
-  match e.getAppArgs.toList with
+  match f.getAppArgs.toList with
   | [] => `(maybeApplied|$fnS:term)
   | [x] => do
       let xS ← PrettyPrinter.delab x
-      `(maybeApplied|$fnS:term applied to $xS:term)
+      `(maybeApplied|$fnS:term aplicado em $xS:term)
   | s => do
       let mut arr : Syntax.TSepArray `term "," := ∅
       for x in s do
         arr := arr.push (← PrettyPrinter.delab x)
-      `(maybeApplied|$fnS:term applied to [$arr:term,*])
+      `(maybeApplied|$fnS:term aplicado em [$arr:term,*])
 
 implement_endpoint (lang := en) nameAlreadyUsed (n : Name) : CoreM String :=
-pure s!"The name {n} is already used"
+pure s!"TO nome {n} já está em uso."
 
-implement_endpoint (lang := en) notDefEq (e val : MessageData) : CoreM MessageData :=
-pure m!"Given term{e}\nis not definitionally equal to the expected{val}"
+implement_endpoint (lang := en) notDefEq (f val : MessageData) : CoreM MessageData :=
+pure m!"O termo {f}\nNão é intensionamente igual ao {val} esperado."
 
 implement_endpoint (lang := en) notAppConst : CoreM String :=
-pure "Not an application of a definition."
+pure "Não é uma aplicação de definição."
 
 implement_endpoint (lang := en) cannotExpand : CoreM String :=
-pure "Cannot expand head."
+pure "Não foi possível expandir."
 
 implement_endpoint (lang := en) doesntFollow (tgt : MessageData) : CoreM MessageData :=
-pure m!"The following does not seem to follow immediately from at most one local assumption: {tgt}"
+pure m!"O termo {tgt} não parece ser inferível a partir de no máximo um dado local."
 
 implement_endpoint (lang := en) couldNotProve (goal : Format) : CoreM String :=
-pure s!"Could not prove:\n{goal}"
+pure s!"Não foi possível demonstrar:\n{goal}"
 
 implement_endpoint (lang := en) failedProofUsing (goal : Format) : CoreM String :=
-pure s!"Failed to prove this using the provided facts.\n{goal}"
+pure s!"Falha ao demonstrar isso usando os fatos providos.\n{goal}"
